8cf38a0d8fb17b53205ec41ceeca1b3bf1ea47c8
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-02-07 15:17:05 +0200

    nigga?

--- Thearchitets_nigga_proj.ino
+++ Thearchitets_nigga_proj.ino
@@ -1,14 +1,14 @@
 #include <IRremote.h>
 
-void yo() {
+void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
-void huh() {
-  Serial.write("nigga\n"); 
+void loop() {
+  Serial.write("Hello?\n"); 
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(50);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
